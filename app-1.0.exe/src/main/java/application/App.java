17131dde749eb4f7261e7b090fe4c28acbdf030e
bmalkmus <bmalkmus23@gmail.com>
2020-09-03 18:54:08 -0700

    added event handler to button

--- app-1.0.exe/src/main/java/application/App.java
+++ app-1.0.exe/src/main/java/application/App.java
@@ -1,13 +1,15 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-public class App extends Application {
+public class App extends Application implements EventHandler<ActionEvent>{
     Button button;
     
     public static void main(String[] args) {
@@ -17,7 +19,8 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Test Application");
-        button = new Button("This is a new button");
+        button = new Button("Click for the Magic Password");
+        button.setOnAction(this);
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
@@ -25,6 +28,14 @@ public class App extends Application {
         Scene scene = new Scene (layout, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource()==button){
+            System.out.println("Magic Password: ****************");
+        }
 
     }
 
