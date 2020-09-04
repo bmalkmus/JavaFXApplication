44f6ad684f99e7a7166651cf0eb73d51916b9e7b
bmalkmus <bmalkmus23@gmail.com>
2020-09-04 00:04:47 -0700

    doubel scene window

--- app-1.0.exe/src/main/java/application/App.java
+++ app-1.0.exe/src/main/java/application/App.java
@@ -3,12 +3,17 @@ package application;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+// import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class App extends Application {
-    Button button;
+    Stage window;
+    Scene scene1;
+    Scene scene2;
+
     
     public static void main(String[] args) {
         launch(args);
@@ -16,16 +21,41 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Test Application");
-        button = new Button("Click for the Magic Password");
-        button.setOnAction(e -> System.out.println("Magic Password: **********"));
+        window = primaryStage;
+        Label label1 = new Label("Welcome to page one");
+        Button button1 = new Button ("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        Label label2 = new Label ("Welcome to page two");
 
-        Scene scene = new Scene (layout, 300, 300);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Button button2 = new Button ("Go to scene 1");
+        button2.setOnAction(e->window.setScene(scene1));
+
+
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+
+        scene1= new Scene (layout1, 200, 200);
+
+        VBox layout2 = new VBox(20);
+        layout2.getChildren().addAll(label2, button2);
+
+        scene2=new Scene (layout2, 200, 200);
+
+        window.setScene(scene1);
+        window.setTitle("Window scene switch");
+        window.show();
+        // primaryStage.setTitle("Test Application");
+        // button = new Button("Click for the Magic Password");
+        // button.setOnAction(e -> System.out.println("Magic Password: **********"));
+
+        // StackPane layout = new StackPane();
+        // layout.getChildren().add(button);
+
+        // Scene scene = new Scene (layout, 300, 300);
+        // primaryStage.setScene(scene);
+        // primaryStage.show();
+
 
     }
 
