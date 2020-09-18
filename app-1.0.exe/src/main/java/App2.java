815f51608e4fad6b46103271734c50e0e26ea338
bmalkmus <bmalkmus23@gmail.com>
2020-09-17 20:29:21 -0700

    functionality to the menu items

--- app-1.0.exe/src/main/java/App2.java
+++ app-1.0.exe/src/main/java/App2.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -19,17 +20,35 @@ public class App2 extends Application {
         window = primaryStage;
         window.setTitle("Menu Creation");
 
-        //file menu
+        // file menu
         Menu fileMenu = new Menu("File");
 
-        //items for menu
-        fileMenu.getItems().add(new MenuItem("New Project..."));
-        fileMenu.getItems().add(new MenuItem("New Module..."));
-        fileMenu.getItems().add(new MenuItem("Import Project..."));
+        // items for menu
+        MenuItem newFile = new MenuItem("New...");
+        newFile.setOnAction(e -> System.out.println("Creating a new window...."));
+        fileMenu.getItems().add(newFile);
+        fileMenu.getItems().add(new MenuItem("Open..."));
+        fileMenu.getItems().add(new MenuItem("Save..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(new MenuItem("Settings..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(new MenuItem("Exit"));
+
+        //Edit Menu
+
+        Menu editMenu = new Menu ("_Edit");
+        editMenu.getItems().add(new MenuItem("Cut"));
+        editMenu.getItems().add(new MenuItem("Copy"));
+        editMenu.getItems().add(new MenuItem("Crop"));
+        editMenu.getItems().add(new MenuItem("Paste"));
+        MenuItem rotate = new MenuItem("Rotate");
+        rotate.setOnAction(e->System.out.println("Rotating due to the spins"));
+        rotate.setDisable(true);
+        editMenu.getItems().add(rotate);
 
         //main menu-bar
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu);
+        menuBar.getMenus().addAll(fileMenu, editMenu);
 
 
         layout = new BorderPane();  
